05a5c76b29254114955a196849e59c28ced276e3
Pere Villega <pere.villega@gmail.com>
2012-11-03 20:00:06 +0000

    Added working WebSockets interface, to be updated later once we have the stream parsing data properly

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,12 +1,30 @@
 package controllers
 
 import play.api._
+import libs.json.JsValue
 import play.api.mvc._
+import play.api.Play.current
+import models.WebClient
 
+/**
+ * Main interface for the web page
+ */
 object Application extends Controller {
-  
-  def index = Action {
-    Ok(views.html.index("Your new application is ready."))
+
+  private lazy val IS_WEB_ENABLED = Play.configuration.getBoolean("enable.webInterface").getOrElse(false)
+
+  def index = Action { implicit request =>
+    if(IS_WEB_ENABLED) {
+      //Concurrent.hub()
+      Ok(views.html.index())
+    } else {
+      NotFound
+    }
+  }
+
+
+  def readStreamData =  WebSocket.async[JsValue] { request =>
+      WebClient.start(request.remoteAddress)
   }
   
 }
